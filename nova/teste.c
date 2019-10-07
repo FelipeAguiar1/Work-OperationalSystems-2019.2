4f3a2e503163ffe0e90800ded695125d8677ab70
Felipe Aguiar <34357176+FelipeAguiar1@users.noreply.github.com>
2019-10-07 15:42:48 -0300

    Update teste.c

--- nova/teste.c
+++ nova/teste.c
@@ -3,11 +3,20 @@
 #include <dirent.h>
 
 int main(void) {
-
+    
+    char *teste = (char*)malloc(100);
+    char *aux = (char*)malloc(100);
     DIR *dir;
     struct dirent *lsdir;
+    
+    strcpy(teste, "c:/");
+    
+    scanf("%s", aux);
+    
+    strcat(teste, aux);
+    strcat(teste, "/");
 
-    dir = opendir("c:\\Usuários\\");
+    dir = opendir(teste);
 
     /* print all the files and directories within directory */
     while ( ( lsdir = readdir(dir) ) != NULL )
