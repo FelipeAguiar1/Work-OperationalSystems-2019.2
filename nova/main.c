a622ef6433d043404f88b2fc3199d46ab18eccde
Felipe Aguiar <34357176+FelipeAguiar1@users.noreply.github.com>
2019-10-07 15:39:14 -0300

    Add files via upload

---
+++ nova/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+int main(void)
+{
+
+    DIR *dir;
+    struct dirent *lsdir;
+
+    dir = opendir("c:\\Windows\\");
+
+    /* print all the files and directories within directory */
+    while ( ( lsdir = readdir(dir) ) != NULL )
+    {
+        printf ("%s\n", lsdir->d_name);
+    }
+
+    closedir(dir);
+
+    return 0;
+
+}
