d286f9123e5f8d34db9ba7cbba1bac25abf0faa6
Felipe Aguiar <34357176+FelipeAguiar1@users.noreply.github.com>
2019-10-15 22:05:47 -0300

    Update teste.c

--- nova/teste.c
+++ nova/teste.c
@@ -1,144 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <unistd.h>
 #include <string.h>
 
-int ProcDir(char* palavra, DIR* diretorio) {
-    char* item_diretorio;
-
-    if (!diretorio ) {       
-        perror( "opendir" );
-    }   
-    
-    item_diretorio = (char*) readdir( diretorio );  
-    
-    while( item_diretorio ) {
-        if (palavra==item_diretorio) {
-            return 1;
-            break;
-        }       
-        item_diretorio = (char*) readdir( diretorio );  
-    }   
-
-    return 0;
-    closedir( diretorio );   
+int ProcDir(char* palavra, char* diretorio) {
+	struct dirent **namelist; //Porque 2 ** ?
+   	int n = scandir(diretorio, &namelist, NULL, alphasort); //alphasort é uma função, assim como scandir
+   	//printf("%d\n", n);
+   	while (n--) {
+		//printf("%s\n", namelist[n]->d_name);
+		if (strcmp(namelist[n]->d_name, palavra)==0) {
+			return 1;
+		}
+		free(namelist[n]);
+   	}
+   	free(namelist); //Queria entender como funciona o free
+   	return 0;
 }
 
-char* MudaDir(char *palavra, DIR* diretorio) {
-    char *copia = palavra;
-    int op;
-    op = ProcDir(copia, diretorio);
-    if (op==1) {
-     //   char *texto = (char*)malloc(100);
-        char *exe = (char*)malloc(100);
+int Menu() {
+	int operacao;
 
-    //    printf("Digite algo: ");
-    //    scanf("%[^\n]", texto);
+	printf("OPERAÇÕES DISPONIVEIS:\n");
+	printf("PWD [1] / CD [2] / LS [3] / MORE [4] / GREP [5] \n");
+	printf("Digite a operação: ");
+	scanf("%d", &operacao);
 
-        for (int i = 3; i<strlen(palavra); i++) {
-            exe[i-3] = palavra[i];
-        }
-
-        printf("diretorio atual %s\n", exe);
-
-        return exe;    
-    }
-
-    return NULL;
+	return operacao;
 }
 
-void ListarDir(char* palavra) {
-    printf("entrou\n");
-    DIR* diretorio;
-    struct dirent* item_diretorio;
-    diretorio = opendir(palavra);
-
-    if (!diretorio ) {
-        perror( "opendir" );
-    }
-    
-    item_diretorio = readdir( diretorio );  
-    
-    while( item_diretorio ) {       
-        printf( "Item = %s\n", item_diretorio->d_name );        
-        item_diretorio = readdir( diretorio );  
-    }   
-    
-    closedir( diretorio );    
+void MostrarDir(char *dir) {
+	if ( fork()==0 ) execlp("/bin/ls", "/bin/ls", dir, NULL) ;
+    	else {
+	    	sleep(1) ;  /* espera o fim de ls para executar o printf() */
+	    	//printf ("Eu sou o pai e finalmente posso continuar\n") ;
+	    }
+   // exit(0);
 }
 
-int main() {   
-    char *operacao = (char*)malloc(100); 
-    DIR* diretorio; 
-    struct dirent* item_diretorio;
-    char *teste = "/";
-    char* teste1 = (char*)malloc(100);
-    diretorio = opendir(teste);
+void MostrarArq(char* dir, char* arq) {
+	int op = ProcDir(arq, dir);
 
-    printf("Diretorio atual %s\n", teste);
-    printf("Digite a operacao: ");
-    scanf("%s", operacao);
-    int op = strcmp(operacao, "ls");
-    printf("numero %d\n", op);
-
-    if (op==0) {
-        printf("foi\n");
-        ListarDir(teste);
-    } else {
-        printf("Diretorio: ");
-        scanf("%s", teste1);
-        teste1 = MudaDir(teste1, diretorio);
-        strcat(teste, teste1);
-    }
-
-/*
-    if (!diretorio ) {       
-        perror( "opendir" );        
-        return( -1 );
-    }   
-    
-    item_diretorio = readdir( diretorio );  
-    
-    while( item_diretorio ) {       
-        printf( "Item = %s\n", item_diretorio->d_name );        
-        item_diretorio = readdir( diretorio );  
-    }   
-    
-    closedir( diretorio );  
-    return( 0 );
-    */
+	if (op==1) {
+		if ( fork()==0 ) execl("/bin/more", "more", arq, NULL);
+	    	else {
+		    	sleep(1) ;  /* espera o fim de ls para executar o printf() */
+		    	//printf ("Eu sou o pai e finalmente posso continuar\n") ;
+		    }
+	} else {
+		printf("Arquivo não existe no diretorio\n");
+	}
 }
-/*
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <dirent.h>
 
-int main(void) {
-    char *teste = (char*)malloc(100);
-    char *aux = (char*)malloc(100);
-    DIR *dir;
-    struct dirent *lsdir;
-    
-   // strcpy(teste, ":/");
-    
-   // printf("Digite algo ");
-    // scanf("%s", aux);
-    
-   // strcat(teste, aux);
-   // strcat(teste, "/");
+int main() {
+	DIR* diretorio;
+	int verifica = 1;
+	char dir[100] = "/home/felipeaguiar/Documentos";
+	diretorio = opendir(dir);
+	int operacao;
+	char *op = (char*)malloc(100*sizeof(char));
 
-    dir = opendir(":/");
+	while (verifica==1) {
+		operacao = Menu();
+		switch(operacao) {
+			case 1:
+				printf("\n");
+				printf("Diretorio atual: %s\n", dir);
+				printf("\n");
+			break;
 
-    /* print all the files and directories within directory */
-/*
-    while ( ( lsdir = readdir(dir) ) != NULL )
-    {
-        printf ("%s\n", lsdir->d_name);
-    }
+			case 2:
+				printf("cd ");
+				scanf("%s", op);
+				int proc = ProcDir(op, dir);
+				if (proc==1) {
+					strcat(op, "/");
+					strcat(dir, op);
+					printf("\n");
+					printf("Diretorio atual: %s\n", dir);
+				}
+			break;
 
-    closedir(dir);
+			case 3:
+				MostrarDir(dir);
+			break;
 
-    return 0;
+			case 4:
+				printf("more ");
+				scanf("%s", op);
+				MostrarArq(dir, op);
+			break;
+		}
+		printf("DESEJA CONTINUAR: SIM [1] / NÃO [Qualquer tecla] \n");
+    	scanf("%d", &verifica);
+	}
 }
-*/
